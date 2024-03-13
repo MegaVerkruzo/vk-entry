5f40b40a0558a4002a282775579ebb22f4415da5
Alexey Grunskii <robochat@mail.ru>
2024-03-13 16:14:14 +0300

    Save files

---
+++ src/main/java/org/grunskii/vkentry/VkEntryApplication.java
@@ -0,0 +1,13 @@
+package org.grunskii.vkentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkEntryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkEntryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grunskii/vkentry/VkEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grunskii.vkentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkEntryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
